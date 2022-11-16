9bb542189ab3b7f07e408090b97fd72302c364d3
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-11-17 01:23:25 +0200

    sum

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.startButton.addEventListener('click', onButtonStart);
+refs.stopButton.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  refs.startButton.setAttribute('disabled', 'disabled');
+  refs.stopButton.removeAttribute('disabled');
+
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onButtonStop() {
+  refs.startButton.removeAttribute('disabled');
+  refs.stopButton.setAttribute('disabled', 'disabled');
+
+  clearInterval(timerId);
+}
