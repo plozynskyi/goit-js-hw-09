bbb7f672197af29ceb31ba4892c3de4349bb7f18
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-11-17 13:48:26 +0200

    sum

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const options = {
   enableTime: true,
@@ -13,6 +14,28 @@ const options = {
 
 refs = {
   input: document.querySelector('#datetime-picker'),
+  seconds: document.querySelector('[data-seconds]'),
+  minutes: document.querySelector('[data-minutes]'),
+  hours: document.querySelector('[data-hours]'),
+  days: document.querySelector('[data-days]'),
 };
 
 flatpickr(refs.input, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+convertMs(onClose);
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
