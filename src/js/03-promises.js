cd016fae8f982dc7c6c45e07572628f02929e0ad
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2022-11-23 17:39:49 +0200

    sum

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,17 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  button: document.querySelector('button'),
+};
+
+console.log(refs.form);
+console.log(refs.delay);
+console.log(refs.step);
+console.log(refs.amount);
+console.log(refs.button);
